ac0a848fee53d519bf5f288b7c5927924d23683b
Jean Cedric ATT <berenger.attiembonon@gmail.com>
2018-01-22 08:04:52 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,19 @@ import express from 'express';
 const router = express.Router();
 var request = require('request');
 
-/* GET index page. */
+/* index page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Bitcoin For Beginners'
+    title: 'Follow bitcoin'
   });
 });
 
-// router.get('/currency', (req, res, next) => {
-//   var coins = request({
-//     uri: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
-//   });
-//   res.send('currency', {coins});
-// });
+router.get('/test', (req, res, next) => {
+  var coins = request({
+    uri: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+  }).pipe(res);
+  console.log(coins)
+});
 
 router.get('/currency', (req, res, next) => {
   request("https://api.coinmarketcap.com/v1/ticker/?limit=10", function(err,res,body) {
@@ -28,14 +28,6 @@ router.get('/currency', (req, res, next) => {
 });
 
 
-
-router.get('/test', (req, res, next) => {
-  var coins = request({
-    uri: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
-  }).pipe(res);
-  console.log(coins)
-});
-
 export default router;
 
-// .pipe(res)
+
